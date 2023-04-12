5d1d590c61a20e8ff10264c92cf264a3a15b633e
root <root@LAPTOP-TDB0OHP5>
2023-04-12 21:46:08 +0300

    some updates about reserve airports

--- Main.cpp
+++ Main.cpp
@@ -13,9 +13,25 @@ int main() {
     Airports vnukovo("Vnukovo", "Moscow", 1.00, 2.000, 1);
     Airports monaco("Monaco", "Monaco", 12.00, 22.000, 1);
 // запускается функция рассчета куда можно полететь туда и полетим
+    Airports destination_airport();
     destination_airport = where_we_go(vnukovo.latitude, vnukovo.longitude, first);
+    vector<Airports> reserve_airports;  // запасные аэропорты
+    reserve_airports = algo_reserve_airports(vnukovo.latitude, vnukovo.longitude, destination_airport.latitude, destination_airport.longitude);
 //запускается процедура полета в каждый момент времени известны координаты
-    fly(vnukovo, first, destination_airport);
+    while (currently_lat != destination_airport.latitude && currently_lon != destination_airport.longitude) {
+        if (currently_weather_in_destination != 0) {
+            fly(first, destination_airport);
+        } else {
+            reserve_airports = algo_reserve_airports(currently_lat, currently_lon, destination_airport.latitude, destination_airport.longitude); // это перерасчет
+            if (reserve_airports.size() == 3)
+                destination_airport = reserve_airports[2];
+            else if (reserve_airports.size() == 2)
+                destination_airport = reserve_airports[1];
+            else if (reserve_airports.size() == 1)
+                destination_airport = reserve_airports[0];
+        }
+    }
+
 //если погода плоха, то запускается перерасчет
 //после перерасчета процедура полета продолжается до тех пор пока саолет не долетит
     return 0;
--- where_we_go.cpp
+++ where_we_go.cpp
@@ -7,9 +7,10 @@ Airports where_we_go(float currently_lat, float currently_lon, Planes plane) {
     float time_left = plane.fuel_left / plane.fuel_consumption;
     float range = plane.speed * time_left; // сколько км осталось в запасе у самолета
     vector<Airports> airports_where_we_can_go;
-    for (int i = 0; i < spisok_airports; i++) {
-        if (pow(currently_lat - spisok_airports[i].latitude, 2) + pow(currently_lat - spisok_airports[i].latitude, 2) < pow(range - 1000, 2)) {
-            airports_where_we_can_go.push_back(spisok_airports[i]);
+    data_base_airports // из бд все аэропорты
+    for (int i = 0; i < data_base_airports; i++) {//добавляем токо те в которых хорошая погода и по пкти к которым есть запасные
+        if (pow(currently_lat - data_base_airports[i].latitude, 2) + pow(currently_lon - data_base_airports[i].longitude, 2) < pow(range - 1000, 2)) {
+            airports_where_we_can_go.push_back(data_base_airports[i]);
         }
     }
     //список аэропортов куда можно лететь пополнен осталось выбрать куда хотим лететь
@@ -20,7 +21,30 @@ Airports where_we_go(float currently_lat, float currently_lon, Planes plane) {
     cin >> destination_airport;
     return destination_airport;
 }
-
+vector<Airports> algo_reserve_airports(float x1, float y1, float x2, float y2) {
+    vector<Airports> reserve_airports;
+    int i = 0;
+    bool f1 = 0, f2 = 0, f3 = 0;
+    while(reserve_airports.size() < 3) {
+        float x = data_base_airports[i].latitude;
+        float y = data_base_airports[i].longitude;
+        if (!f1 && x1 < x && x < (x2 - x2)/3 && ) {
+            reserve_airports.push_back(data_base_airports[i]);
+            f1 = 1;
+            i++;
+        } else if (!f2 && (x2 - x2)/3 < x && x < 2*(x2 - x2)/3 && ) {
+            reserve_airports.push_back(data_base_airports[i]);
+            f2 = 1;
+            i++;
+        } else if (!f3 && 2*(x2 - x2)/3 < x && x < x2 && ) {
+            reserve_airports.push_back(data_base_airports[i]);
+            f3 = 1;
+            i++;
+        } else
+            i++;
+    }
+    return reserve_airports;
+}
 /*
 double lat1 = 51.5007; double lon1 = 0.1246;
     double lat2 = 40.6892; double lon2 = 74.0445;
